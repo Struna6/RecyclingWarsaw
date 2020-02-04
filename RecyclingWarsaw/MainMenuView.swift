7b65bf8956f40bb17edbd40cf22aa275c68119f8
Aleksandra Konopka <charlie.warhammer@gmail.com>
2020-02-04 14:11:29 +0100

    darkening buttons when they are pressed

--- RecyclingWarsaw/MainMenuView.swift
+++ RecyclingWarsaw/MainMenuView.swift
@@ -28,8 +28,6 @@ class MainMenuView : UIView{
         super.init(frame: frame)
         setupView()
         
-        print(frame.width)
-        print(frame.height)
         button1 = UIButton(frame: .zero)
         button2 = UIButton(frame: .zero)
         button3 = UIButton(frame: .zero)
@@ -69,7 +67,7 @@ class MainMenuView : UIView{
         button5.setImage(UIImage(named: "Papier"), for: .normal)
         button6.setImage(UIImage(named: "Wielkogabarytowe"), for: .normal)
         button7.setImage(UIImage(named: "Zmieszane"), for: .normal)
-
+        
         button1.imageView?.contentMode = .scaleAspectFit
         button2.imageView?.contentMode = .scaleAspectFit
         button3.imageView?.contentMode = .scaleAspectFit
@@ -78,6 +76,14 @@ class MainMenuView : UIView{
         button6.imageView?.contentMode = .scaleAspectFit
         button7.imageView?.contentMode = .scaleAspectFit
         
+        button1.setBackgroundColor(color: (button1.backgroundColor?.darker())!, forState: UIControl.State.highlighted)
+        button2.setBackgroundColor(color: (button2.backgroundColor?.darker())!, forState: UIControl.State.highlighted)
+        button3.setBackgroundColor(color: (button3.backgroundColor?.darker())!, forState: UIControl.State.highlighted)
+        button4.setBackgroundColor(color: (button4.backgroundColor?.darker())!, forState: UIControl.State.highlighted)
+        button5.setBackgroundColor(color: (button5.backgroundColor?.darker())!, forState: UIControl.State.highlighted)
+        button6.setBackgroundColor(color: (button6.backgroundColor?.darker())!, forState: UIControl.State.highlighted)
+        button7.setBackgroundColor(color: (button7.backgroundColor?.darker())!, forState: UIControl.State.highlighted)
+    
     }
     func setUpLeftVerticalStackView(){
         leftVerticalStackView = UIStackView()
@@ -120,5 +126,41 @@ class MainMenuView : UIView{
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UIColor {
+
+    func lighter(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: abs(percentage) )
+    }
+
+    func darker(by percentage: CGFloat = 30.0) -> UIColor? {
+        return self.adjust(by: -1 * abs(percentage) )
+    }
+
+    func adjust(by percentage: CGFloat = 30.0) -> UIColor? {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            return UIColor(red: min(red + percentage/100, 1.0),
+                           green: min(green + percentage/100, 1.0),
+                           blue: min(blue + percentage/100, 1.0),
+                           alpha: alpha)
+        } else {
+            return nil
+        }
+    }
+}
+extension UIButton {
+    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+        self.clipsToBounds = true  // add this to maintain corner radius
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+            let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            self.setBackgroundImage(colorImage, for: forState)
+        }
     }
 }
